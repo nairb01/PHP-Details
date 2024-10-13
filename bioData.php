1277acd390a50a487a81c0e69922010d6caf0354
BRIAN <115349446+nairb01@users.noreply.github.com>
2024-10-13 23:56:22 +0300

    Add files via upload

---
+++ bioData.php
@@ -0,0 +1,34 @@
+Question 1: <br>
+<?php
+echo "NAME: ";
+echo "Brian Kioko <br>";
+echo "Reg No: ";
+echo "CIT-223-037/2022<br>";
+echo "Computer scientist";
+?>
+<br>
+<br>
+
+Question 2: <br>
+single line comment is not correctly specified<br>
+echo "Display second line  "    //there is no semi colon at the end</br>
+we use echo instead of print <br><br>
+
+3:correct code is <br>
+<?php
+echo "Display first line </br>";
+echo "Display second line </br> ";
+echo "Display third line with print statement ";
+?><br><br>
+
+4: output<br>
+Display first line<br>
+Display second line<br>
+Display third line with print statement<br>
+<br>
+
+5: <?php
+echo "<h3>PHP CODE</h3>"; 
+echo "<p>Hello World</p>";
+?>
+
---
+++ courseDetails.php
@@ -0,0 +1,9 @@
+
+<?php
+echo '<div style="text-align: center;">';
+echo "CIT-223-037/2022 <br>" ;
+
+echo "Computer Science <br>";
+
+echo "Web-development";
+?>
